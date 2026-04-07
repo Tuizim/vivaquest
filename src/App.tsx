3f0d27e6412129ec3d1bb50dce51bb0a1e7fa18c
Artur trombim <arturtrombim.dev@gmail.com>
2026-04-06 22:51:20 -0300

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Onboarding from './pages/Onboarding'
 
 function App() {
   return (
-    <>
-     <h1>Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element=<Navigate to="/onboarding"/>/>
+        <Route path="/onboarding" element=<Onboarding/>/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     VitePWA({ registerType: 'autoUpdate' }),
+    tsconfigPaths(),
   ],
 })
