302e369b927c151a2f18330b73beb18baad02198
Artur trombim <arturtrombim.dev@gmail.com>
2026-05-07 23:03:16 -0300

    add fadescroll to dashboard

--- src/components/dashboard/DashboardHabits.tsx
+++ src/components/dashboard/DashboardHabits.tsx
@@ -2,23 +2,26 @@ import { Flex, Heading, Text } from "@chakra-ui/react"
 import { ConcludeHabit } from "../habits/ConcludeHabit"
 import { useAppStore } from "../../store/useAppStore"
 import { Link } from "react-router-dom"
+import { FadeScroll } from "../layout/FadeScroll"
 
 export function DashboardHabits() {
-  const{habits,toggleHabit} = useAppStore()
+  const { habits, toggleHabit } = useAppStore()
 
   return (
-    <Flex as='section'  w="full" maxW="sm" flexDir="column" gap="3" p="3">
+    <Flex as='section' w="full" maxW="sm" flexDir="column" gap="3" p="3">
       <Flex align="center" justify="space-between" mt={5}>
         <Heading as="h3" size="xl">Seus hábitos</Heading>
         <Link to="/habits-manager">
           <Text fontSize="sm" color="brand.500" fontWeight="normal">Outras opções</Text>
         </Link>
       </Flex>
-      {
-        habits.map(habit => (
-          <ConcludeHabit habit={habit} key={habit.id} toggleHabit={toggleHabit}></ConcludeHabit>
-        ))
-      }
+      <FadeScroll maxH="calc(100dvh - 230px)">
+        {
+          habits.map(habit => (
+            <ConcludeHabit habit={habit} key={habit.id} toggleHabit={toggleHabit}></ConcludeHabit>
+          ))
+        }
+      </FadeScroll>
     </Flex>
   )
 }
